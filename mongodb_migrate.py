4c0f2392f5f84a0825a16250b5c347e467876be9
0xBolt <74103155+GitBolt@users.noreply.github.com>
2022-02-23 00:19:44 +0530

    🔒 Add exception catching

--- mongodb_migrate.py
+++ mongodb_migrate.py
@@ -1,11 +1,14 @@
 """Simple script to migrate all data from one MongoDB instance to other"""
 from pymongo import MongoClient
 
-client1 = MongoClient("mongodb://...")
-client2 = MongoClient("localhost:27017")
+client1: MongoClient = MongoClient("mongodb://...")
+client2: MongoClient = MongoClient("localhost:27017")
 
 for db in client1.list_database_names():
     for col in client1[db].list_collection_names():
         for data in client1[db][col].find({}):
-            client2[db][col].insert_one(data)
-            print(db, "-", col
+            try:
+                client2[db][col].insert_one(data)
+                print(db, "-", col)
+            except Exception as e:
+                print(db, "-", col, "\n", e)
